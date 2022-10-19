fd3d6da87e0c3c2f8b019539276e459a38ad18a6
simhyunbo <hbo9347@naver.com>
2022-10-19 10:23:10 +0900

    Stack

---
+++ src/main/java/Stack01.java
@@ -0,0 +1,21 @@
+public class Stack01 {
+
+
+        private int[] arr = new int[10000];
+        private int pointer = 0;
+        public Stack01(){
+
+        }
+        public Stack01(int size){
+            this.arr = new int[size];
+        }
+
+        public void push(int value){
+        this.arr[this.pointer] = value;
+        this.pointer++;
+    }
+
+    public int[] getArr(){
+            return arr;
+    }
+}
