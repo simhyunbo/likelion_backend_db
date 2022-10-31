d6353dc748ae371042e8bda2acc74adaa056a475
simhyunbo <hbo9347@naver.com>
2022-10-31 09:41:59 +0900

    모의고사

---
+++ src/main/java/hash/programmers/PreparedTest.java
@@ -0,0 +1,11 @@
+package hash.programmers;
+
+public class PreparedTest {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
