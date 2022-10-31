7caea6b6ab513dc6ee6209ec4fb6264dfe8a1d03
simhyunbo <hbo9347@naver.com>
2022-10-31 10:01:01 +0900

    모의고사

--- src/main/java/hash/programmers/PreparedTest.java
+++ src/main/java/hash/programmers/PreparedTest.java
@@ -3,9 +3,18 @@ package hash.programmers;
 public class PreparedTest {
     public int[] solution(int[] answers) {
         int[] answer = {};
+        int[] answer1 = {1,2,3,4,5};
+        int[] answer2 = {2,1,2,3,2,4,2,5};
+        int[] answer3 = {3,3,1,1,2,2,4,4,5,5};
+
+        //배열 반복하기
+
+
+
         return answer;
     }
     public static void main(String[] args) {
+
 
     }
 }
