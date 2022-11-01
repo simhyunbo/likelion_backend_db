77c887b8a095de3f164caebe7d7b5f9b8877aac1
simhyunbo <hbo9347@naver.com>
2022-11-01 10:48:47 +0900

    Interface 적용

--- src/main/java/hash/programmers/TemplateCallbackPrime.java
+++ src/main/java/hash/programmers/TemplateCallbackPrime.java
@@ -1,15 +1,21 @@
 package hash.programmers;
 
+interface StatementStrategy{
+    //인터페이스 이용
+    boolean compare(int a , int b);
+}
+
 public class TemplateCallbackPrime {
+    //Template Callback
     boolean someOperation(int a, int b) {
         return a < b;
     }
 
-    boolean isPrime(int num){
+    boolean isPrime(int num,StatementStrategy stmt){
         // i < num
         // i < num/2
         // i*i < num  i * i 쓰는이유 i*i<num == i<Math.sqrt(num)
-        for (int i = 2; someOperation(i,num); i++) {
+        for (int i = 2; stmt.compare(i,num); i++) {
             if(num%i == 0 ) return false;
         }
         return true;
@@ -17,7 +23,16 @@ public class TemplateCallbackPrime {
 
     public static void main(String[] args) {
         TemplateCallbackPrime tcp = new TemplateCallbackPrime();
-        boolean b = tcp.isPrime(17);
-        System.out.println(b);
+        System.out.println(tcp.isPrime(13, (a, b)-> a < b));
+        System.out.println(tcp.isPrime(17, (a, b)-> a < b/2));
+        System.out.println(tcp.isPrime(19, (a, b)-> a * a < b));
+
+//            @Override
+//        boolean b = tcp.isPrime(17, new StatementStrategy() {
+//            public boolean compare(int a, int b) {
+//                return a * a <= b;
+//            }
+//        });
+
     }
 }
