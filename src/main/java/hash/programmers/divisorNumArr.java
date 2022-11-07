f7dd76e2c8b0c6122b3193bb111863a1654d5b92
simhyunbo <hbo9347@naver.com>
2022-11-07 09:55:30 +0900

    나누어 떨어지는 숫자 배열 - 조건 %해서 0이면 list.add()

---
+++ src/main/java/hash/programmers/divisorNumArr.java
@@ -0,0 +1,26 @@
+package hash.programmers;
+
+import java.util.ArrayList;
+
+public class divisorNumArr {
+    //조건 %해서 0이면 list.add()
+    // 오름차순으로 정렬
+    // list 개수가 0이면 [-1], [5,10]
+
+        public int[] solution(int[] arr, int divisor) {
+            int[] answer = {};
+            ArrayList<Integer> arrNum = new ArrayList<Integer>();
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i] % divisor == 0){
+                    arrNum.add(arr[i]);
+                }
+            }
+
+            return answer;
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
