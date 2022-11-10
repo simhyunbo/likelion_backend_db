a063500c455921d49bbf458478631484ee2c51cf
simhyunbo <hbo9347@naver.com>
2022-11-10 09:33:03 +0900

    같은 숫자는 싫어

--- src/main/java/hash/programmers/Week8/notSameNumber.java
+++ src/main/java/hash/programmers/Week8/notSameNumber.java
@@ -6,21 +6,16 @@ import java.util.HashSet;
 
 public class notSameNumber {
 
-
-        public int[] solution(int []arr) {
-            ArrayList<Integer> answerList = new ArrayList<Integer>();
-            for (int i = 0; i < arr.length; i++) {
-
-                    answerList.add(arr[i]);
-
-                }
-            return arr;
-
-        }
-
         public static void main(String[] args) {
             int[] arr = {1,1,3,3,0,1,1};
-            notSameNumber narr = new notSameNumber();
-            narr.solution(arr);
+            ArrayList<Integer> answerList = new ArrayList<Integer>();
+            int sameNum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i] != sameNum) {
+                    answerList.add(arr[i]);
+                    sameNum = arr[i];
+                }
+            }
+            System.out.println(answerList);
         }
 }
