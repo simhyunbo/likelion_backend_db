8d3e3fa0add66d57a27a78d110f4272054a555a3
simhyunbo <hbo9347@naver.com>
2022-11-11 09:35:34 +0900

    10진수 2진수 변환 Integer.toBinaryString 진행중

--- src/main/java/hash/programmers/Week8/SecretMap.java
+++ src/main/java/hash/programmers/Week8/SecretMap.java
@@ -15,7 +15,18 @@ public class SecretMap {
         for (int i = 0; i < arr1.length; i++) {
             strArr2[i] = Integer.toBinaryString(arr2[i]);
         }
+        //Integer.toBinaryString 없이 2진법
+        int[] binary = new int[10];
+        int num = 9;
+        int n = 0;
+        while (num > 0) {
+            binary[n] = num % 2;
+            num = num / 2;
+            n++;
+            System.out.println(num);
+        }
 
+        System.out.println(num);
 
 
 
