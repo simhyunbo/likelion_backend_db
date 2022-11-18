aaecc29be0ad59e8dfaf72d971fb51870f51b4c1
simhyunbo <hbo9347@naver.com>
2022-11-18 09:42:57 +0900

    시저 암호 문자 한칸 밀기

--- src/main/java/hash/programmers/Week8/CaesarCipher.java
+++ src/main/java/hash/programmers/Week8/CaesarCipher.java
@@ -5,17 +5,19 @@ public class CaesarCipher {
         //문자열 1칸 밀기
         String answer ="";
         int CaesarNum = 1;
-        String str = "ABCDE";
+        String str = "abcde";
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             System.out.println(ch);
             if(ch>='a' && ch<='z'){
                 if(ch+CaesarNum > 'z'){
-
+                    answer += (char)(ch-26+CaesarNum);
+                }else{
+                    answer += (char)(ch+CaesarNum);
                 }
+                System.out.println(answer);
             }
 
         }
-
     }
 }
