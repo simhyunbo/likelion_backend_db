a78d26a0cf464b10a366367b919c047b581d4b2e
simhyunbo <hbo9347@naver.com>
2022-11-22 09:16:22 +0900

    1부터 100까지 출력

--- src/main/java/hash/programmers/Print1TO100V2.java
+++ src/main/java/hash/programmers/Print1TO100V2.java
@@ -2,11 +2,14 @@ package hash.programmers;
 
 public class Print1TO100V2 {
     public static void print1To100(int num){
-        if (num == 100) {
+        //시작하는 값은 어떤 것인지
+        //변화는 어떻게 줄 것인지 - print1To100(num + 1)
+        //언제 끝낼 것인지
+        if (num == 101) {
             return;
         }
         System.out.println(num);
-        print1To100(num);
+        print1To100(num+1);
     }
     public static void main(String[] args) {
         print1To100(1);
