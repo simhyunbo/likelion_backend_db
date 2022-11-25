a7ac2713cdcf860ab00758fbf28b5229640dccb7
simhyunbo <hbo9347@naver.com>
2022-11-25 09:17:36 +0900

    최대공약수

---
+++ src/main/java/hash/programmers/divisorNum.java
@@ -0,0 +1,14 @@
+package hash.programmers;
+
+public class divisorNum {
+    public int divNum(int a, int b){
+        if(a>b){
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
