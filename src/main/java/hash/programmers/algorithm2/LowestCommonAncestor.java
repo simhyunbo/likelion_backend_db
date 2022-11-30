845c0caba5ef7c5bde65cae123b8fc06f9d355bb
simhyunbo <hbo9347@naver.com>
2022-11-30 09:23:45 +0900

    (재귀함수) 두 노드간의 거리

---
+++ src/main/java/hash/programmers/algorithm2/LowestCommonAncestor.java
@@ -0,0 +1,16 @@
+package hash.programmers.algorithm2;
+
+public class LowestCommonAncestor {
+
+    int LCA(int a , int b){
+        if(a==b) return 0;
+        if(a>b) {
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
