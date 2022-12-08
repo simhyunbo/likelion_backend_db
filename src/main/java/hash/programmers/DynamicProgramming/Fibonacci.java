64f87cae860e2a16372ee6bcd23af152a2e4a017
simhyunbo <hbo9347@naver.com>
2022-12-08 09:26:47 +0900

    다이나믹 프로그래밍

---
+++ src/main/java/hash/programmers/DynamicProgramming/Fibonacci.java
@@ -0,0 +1,16 @@
+package hash.programmers.DynamicProgramming;
+
+public class Fibonacci {
+
+    public static int fib(int n,int[] memo){
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        //n = 40
+        //fib(40) = memo[40]
+        //int[] memo = new int[n + 1];
+
+    }
+}
