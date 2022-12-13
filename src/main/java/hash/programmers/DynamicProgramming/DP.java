3f1a864d4f11f079987d4dd151681c8104db6e28
simhyunbo <hbo9347@naver.com>
2022-12-13 09:23:59 +0900

    dp hint

---
+++ src/main/java/hash/programmers/DynamicProgramming/DP.java
@@ -0,0 +1,19 @@
+package hash.programmers.DynamicProgramming;
+
+class Pair{
+    int left;
+    int right;
+
+    public Pair(int left,int right){
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class DP {
+    public static void main(String[] args) {
+
+        Pair[][] dp = new Pair[4][4];
+
+    }
+}
